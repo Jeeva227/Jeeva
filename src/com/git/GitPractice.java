d5b88eff3e476c9048644f2a13538e26ac5a5787
Jeeva227 <jeevahr22@gmail.com>
2023-07-18 12:49:18 +0530

    message

--- src/com/git/GitPractice.java
+++ src/com/git/GitPractice.java
@@ -4,4 +4,11 @@ public class GitPractice {
 public static void main(String[] args) {
 	System.out.println("This is the Git Practice code");
 }
+public void git() {
+System.out.println("Git branch");
+
+}
+public void gitalter() {
+	System.out.println("Git new");
+}
 }
