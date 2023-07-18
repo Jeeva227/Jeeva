5746eefe390b4fed243cfa59eb42ebfad323b345
Jeeva227 <jeevahr22@gmail.com>
2023-07-18 12:20:51 +0530

    Git Practice code

--- src/com/git/GitPractice.java
+++ src/com/git/GitPractice.java
@@ -4,4 +4,7 @@ public class GitPractice {
 public static void main(String[] args) {
 	System.out.println("This is the Git Practice code");
 }
+public void git() {
+System.out.println("Git branch");
+}
 }
